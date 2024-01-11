4d5fb53366f262010ef1f88058f92ad277c50669
MateuszRostek <17rostek17@gmail.com>
2024-01-11 01:42:03 +0100

    Book model and its repository, service added, also app properties and pom.xml changes

--- src/main/java/book/store/model/Book.java
+++ src/main/java/book/store/model/Book.java
@@ -1,4 +1,32 @@
 package book.store.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String author;
+    @NotNull
+    @Column(unique = true)
+    private String isbn;
+    @NotNull
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+
 }

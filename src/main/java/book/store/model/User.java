9bd1a20a8fd5c1bc3b02a45721f5e1bf91068727
MateuszRostek <17rostek17@gmail.com>
2024-03-06 23:11:48 +0100

    Changed 'Set<Role>' fetch type in User entity - added query in UserRepository

--- src/main/java/book/store/model/User.java
+++ src/main/java/book/store/model/User.java
@@ -2,7 +2,6 @@ package book.store.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -45,7 +44,7 @@ public class User implements UserDetails {
     private String shippingAddress;
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
--- src/main/java/book/store/repository/user/UserRepository.java
+++ src/main/java/book/store/repository/user/UserRepository.java
@@ -3,10 +3,12 @@ package book.store.repository.user;
 import book.store.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("from User u join fetch u.roles where lower(u.email) like lower(:email)")
     Optional<User> findByEmail(String email);
 }

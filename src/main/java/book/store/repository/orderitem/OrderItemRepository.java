3cc6d63d319f573348295f555eff242fd822c72f
MateuszRostek <17rostek17@gmail.com>
2024-03-16 21:38:59 +0100

    Removed redundant methods from order item repository

--- src/main/java/book/store/repository/orderitem/OrderItemRepository.java
+++ src/main/java/book/store/repository/orderitem/OrderItemRepository.java
@@ -1,14 +1,9 @@
 package book.store.repository.orderitem;
 
 import book.store.model.OrderItem;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    List<OrderItem> findAllByOrderId(Long orderId);
-
-    Optional<OrderItem> findByOrderIdAndId(Long orderId, Long id);
 }

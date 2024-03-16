e7ec95efa0343d54668a66f4465f93afa3bc24ad
MateuszRostek <17rostek17@gmail.com>
2024-03-16 21:30:39 +0100

    Small corrections in Order

--- src/main/java/book/store/model/Order.java
+++ src/main/java/book/store/model/Order.java
@@ -1,7 +1,18 @@
 package book.store.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
